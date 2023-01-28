deeb7625824217247889e92244a1393942f8db01
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2023-01-27 23:10:09 -0300

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+btnSing = document.querySelector('#btnS')
+
+// btnSing.addEventListener('mouseover', function(){
+//     btnSing.style.backgroundColor = 'white'
+//     console.log('ok')
+// })
+
+
+// function changeC(){
+//     btnSing.style.backgroundColor = 'white'
+//     console.log('ok')
+// }
